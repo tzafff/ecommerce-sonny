b8ce82b53cdb0fef1ec3852a8676f679c965fa36
tzafff <christzaf99@gmail.com>
2024-12-27 14:21:12 +0200

    Header Component(Sign in), Passkey functionality

--- app/(store)/layout.tsx
+++ app/(store)/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "../globals.css";
 import {ClerkProvider} from "@clerk/nextjs";
+import Header from "@/components/Header";
 
 
 
@@ -18,9 +19,11 @@ export default function RootLayout({
       <ClerkProvider dynamic>
         <html lang="en">
           <body>
-            {children}
-          </body
-            >
+          <main>
+              <Header />
+              {children}
+          </main>
+          </body>
         </html>
       </ClerkProvider>
   );
--- app/(store)/page.tsx
+++ app/(store)/page.tsx
@@ -1,9 +1,10 @@
-
+import {Button} from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
       <h1>Hello</h1>
+        <Button>Click me</Button>
     </div>
   );
 }
