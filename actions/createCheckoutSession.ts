95befe558b23c2b556169a87ca341abebf048864
tzafff <christzaf99@gmail.com>
2024-12-28 19:25:29 +0200

    Add Success Page after purchase.

--- actions/createCheckoutSession.ts
+++ actions/createCheckoutSession.ts
@@ -36,6 +36,13 @@ export async function createCheckoutSession(
         if(customers.data.length > 0) {
             customerId = customers.data[0].id;
         }
+        const baseUrl = process.env.NODE_ENV === 'production'
+            ? `https://${process.env.VERCEL_URL}`
+            : `${process.env.NEXT_PUBLIC_BASE_URL}`
+
+        const successUrl = `${baseUrl}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`
+        console.log(successUrl)
+        const cancelUrl = `${baseUrl}/basket`
 
         const session = await stripe.checkout.sessions.create({
             customer: customerId,
@@ -44,8 +51,8 @@ export async function createCheckoutSession(
             metadata,
             mode: "payment",
             allow_promotion_codes: true,
-            success_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/success?session_id={CHECKOUT_SESSION_ID}&orderNumber=${metadata.orderNumber}`,
-            cancel_url: `${`https://${process.env.VERCEL_URL}` || process.env.NEXT_PUBLIC_BASE_URL}/basket`,
+            success_url: successUrl,
+            cancel_url: cancelUrl,
             line_items: items.map((item) => ({
                 price_data: {
                     currency: "eur",
